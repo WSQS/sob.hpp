80fdced360317cec7796106d9555053d389ccf11
Sophomore <17792626+WSQS@users.noreply.github.com>
2025-12-12 18:03:41 +0800

    fix:type fix

--- include/file_generator.hpp
+++ include/file_generator.hpp
@@ -95,7 +95,7 @@ namespace sopho
     FileEntry make_entry(std::filesystem::path fs_path)
     {
         auto file_content = read_file(fs_path);
-        return FileEntry{.name = fs_path.filename(),
+        return FileEntry{.name = fs_path.filename().string(),
                          .size = file_content.size(),
                          .hash = std::hash<std::string>{}(file_content),
                          .content = std::make_unique<std::string>(std::move(file_content))};
